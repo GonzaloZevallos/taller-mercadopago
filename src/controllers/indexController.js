42f6162f30965bf585c0c09b753933eb8d89e1aa
gonzalozevallos <gonzalo.zev@gmail.com>
2020-10-14 12:40:23 -0300

    add project

---
+++ src/controllers/indexController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    home: (req, res) => {
+        return res.render("index");
+    },
+    detail: (req, res) => {
+        return res.render("detail", { ...req.query });
+    }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const indexController = require("../controllers/indexController");
+
+/* GET home page. */
+router.get('/', indexController.home);
+
+/* GET detail page */
+router.get('/detail', indexController.detail);
+
+module.exports = router;
